8a44fa6b391439da9825099e963079d15251b5f3
SotaNoniwa <surfersou@gmail.com>
2023-07-17 16:11:09 +0900

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -20,6 +21,13 @@ async function main() {
         email: String,
         password: String
     });
+
+    const secret = "Thisisourlittlesecret";
+    userSchema.plugin(encrypt, {
+        secret: secret,
+        encryptedFields: ['password']
+    });
+
 
     const User = mongoose.model("User", userSchema);
 
