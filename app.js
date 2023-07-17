93b06df73094703b9b1fd1702d45152c99f8eb08
SotaNoniwa <surfersou@gmail.com>
2023-07-17 15:37:23 +0900

    Level 1 - username and password only

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+main().catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+    const userSchema = new mongoose.Schema({
+        email: String,
+        password: String
+    });
+
+    const User = mongoose.model("User", userSchema);
+
+    app.get("/", (req, res) => {
+        res.render("home");
+    });
+
+    app.get("/login", (req, res) => {
+        res.render("login");
+    });
+
+    app.get("/register", (req, res) => {
+        res.render("register");
+    });
+
+    app.post("/register", (req, res) => {
+        const newUser = new User({
+            email: req.body.username, // email
+            password: req.body.password
+        });
+
+        newUser.save()
+            .then(() => res.render("secrets"))
+            .catch((err) => console.log(err));
+    });
+
+    app.post("/login", (req, res) => {
+        const username = req.body.username; // email
+        const password = req.body.password;
+
+        User.findOne({
+                email: username
+            })
+            .then((foundUser) => {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                } else {
+                    res.redirect("/login");
+                }
+            })
+            .catch((err) => console.log(err));
+    })
+
+
+    app.listen(3000, () => {
+        console.log("Server's running on port 3000");
+    });
+}
